555781e456fe576faa5bc03c7a83889d7d9b85b8
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-04-10 13:18:26 +0800

    Move from subdomain to context

--- server.js
+++ server.js
@@ -25,67 +25,68 @@ app.use(webpackDevMiddleware(compiler))
 app.use(webpackHotMiddleware(compiler))
 
 app.use('/*', (req, res) => {
-  if (req.subdomains[0] !== 'api') {
-    const reducer = combineReducers(reducers)
-    const store = createStore(reducer)
-    
-    match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
-      if (err) {
-        console.log(err)
-        return res.status(500).end('Internal server error.')
-      }
-
-      if (!renderProps) return res.status(404).end('Not found.')
-
-      const componentHTML = renderToString((
-        <Provider store={ store }>
-          <RouterContext {...renderProps} />
-        </Provider>
-      ))
-      
-      const initialState = store.getState()
-      
-      const HTML = `
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <meta charset="utf-8">
-            <title>Self Serve App Prototype</title>
-          </head>
-          <body>
-            <div id="app"><div>${componentHTML}</div></div>
-            <script type="application/javascript" src="/bundle.js"></script>
-            <script type="application/javascript">
-              window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}
-            </script>
-          </body>
-        </html>
-      `
-
-      res.end(HTML)  
-    })
-  } else {
-    let opts = {
-      url: process.env.URL + req.originalUrl,
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-        'Authorization': process.env.AUTH
-      },
-      body: null
+  const reducer = combineReducers(reducers)
+  const store = createStore(reducer)
+  
+  match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
+    if (err) {
+      console.log(err)
+      return res.status(500).end('Internal server error.')
     }
-    fetch(opts.url, opts)
-      .then((response) => {
-        return response.json()
-      })
-      .then((response) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-        res.status(200).end(JSON.stringify(response))
-      })
+
+    if (!renderProps) return res.status(404).end('Not found.')
+
+    const componentHTML = renderToString((
+      <Provider store={ store }>
+        <RouterContext {...renderProps} />
+      </Provider>
+    ))
+    
+    const initialState = store.getState()
+    
+    const HTML = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>Self Serve App Prototype</title>
+        </head>
+        <body>
+          <div id="app"><div>${componentHTML}</div></div>
+          <script type="application/javascript" src="/bundle.js"></script>
+          <script type="application/javascript">
+            window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}
+          </script>
+        </body>
+      </html>
+    `
+
+    res.end(HTML)
+  })
+})
+
+app.use('/api/*', (req, res) => {
+  let opts = {
+    url: process.env.URL + req.originalUrl,
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+      'Authorization': process.env.AUTH
+    },
+    body: null
   }
+  
+  fetch(opts.url, opts)
+    .then((response) => {
+      return response.json()
+    })
+    .then((response) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+      res.status(200).end(JSON.stringify(response))
+    })
 })
 
 export default app
--- shared/actions/Task.js
+++ shared/actions/Task.js
@@ -33,18 +33,18 @@ export function displayError(error) {
 
 export function fetchItem(uuid) {
   return function(dispatch) {
-    fetch(`${process.env.SERVER_URL}/${uuid}`)
+    fetch(`${process.env.SERVER_URL}/api/${uuid}`)
       .then(r1 => r1.json())
       .then((r1) => {
         
         if (r1.errors) {
           dispatch(displayError(JSON.stringify(r1.errors)))
         } else {
-          fetch(`${process.env.SERVER_URL}/${uuid}/transactions`)
+          fetch(`${process.env.SERVER_URL}/api/${uuid}/transactions`)
             .then(r2 => r2.json())
             .then((r2) => {
               
-              fetch(`${process.env.SERVER_URL}/${uuid}/events`)
+              fetch(`${process.env.SERVER_URL}/api/${uuid}/events`)
                 .then(r3 => r3.json())
                 .then((r3) => {
                   var timeline = [].concat(r2.transactions.map((t) => {
@@ -87,14 +87,14 @@ export function fetchItem(uuid) {
 
 export function fetchAccount(uuid) {
   return function(dispatch) {
-    fetch(`${process.env.SERVER_URL}/transactions?account_id=${uuid}`)
+    fetch(`${process.env.SERVER_URL}/api/transactions?account_id=${uuid}`)
       .then(r1 => r1.json())
       .then((r1) => {
         
         if (r1.errors) {
           dispatch(displayError(JSON.stringify(r1.errors)))
         } else {
-          fetch(`${process.env.SERVER_URL}/batch_transactions?account_id=${uuid}`)
+          fetch(`${process.env.SERVER_URL}/api/batch_transactions?account_id=${uuid}`)
             .then(r2 => r2.json())
             .then((r2) => {
               let transactions = r1.transactions || []
