b4c6db6bfff7cc5c7cb497bb7138fcd6bc1c21d2
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-05-04 11:08:52 +0800

    Change default port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import server from './server'
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 server.listen(PORT, function() {
   console.log('Server started on port:', PORT)
